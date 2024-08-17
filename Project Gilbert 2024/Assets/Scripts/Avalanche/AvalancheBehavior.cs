fe5842a3289f97d9ba539445bfcd6d3f97d08e72
JFerrigan <jakedavidferrigan@gmail.com>
2024-08-17 00:17:31 -0500

    add event for death

--- Project Gilbert 2024/Assets/Scripts/Avalanche/AvalancheBehavior.cs
+++ Project Gilbert 2024/Assets/Scripts/Avalanche/AvalancheBehavior.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.XR;
 using UnityEngine;
-
+using UnityEngine.Events;
 public class AvalancheBehavior : MonoBehaviour
 {
     private Rigidbody2D rb2d;
@@ -11,6 +11,9 @@ public class AvalancheBehavior : MonoBehaviour
     public float MoveSpeed;
     public int MoveDirection = 1;
 
+    private Vector3 defaultPosition;
+    
+
     [Header("Climbing")]
     public bool isClimbing;
 
@@ -18,6 +21,8 @@ public class AvalancheBehavior : MonoBehaviour
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        defaultPosition = rb2d.position;
+        Reset();
     }
 
     // Update is called once per frame
@@ -61,6 +66,23 @@ public class AvalancheBehavior : MonoBehaviour
                 Debug.Log("Player Dead!");
             }
         }
+    }
+
+
+ /**This is the function you select on the GameObject with a UnityEvent. 
+    This event will live on the Player OBJECT (NOT prefab), so click that & view in inspector
+    Scroll down to HealthComponent & see the list of events.
+
+    This is the reset event for the avalanch
+    
+    To add a new one, hit plus, select a GameObject (switch tabs to scene) NOT A PREFAB
+    Then you can select the function on the script component of that GameObject.
+    If it isn't showing up, try adding a pointless argument... IDK why man... IDK...
+    **/
+    public void Reset(){
+        Debug.Log("Resetting avalanche");
+        rb2d.position = defaultPosition;
+        Debug.Log(defaultPosition);
     }
     
 }
