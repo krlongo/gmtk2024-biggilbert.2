ba0cd8d1703e6947133ee01cc6cceac9e91b5c27
Kevon Long <krlong@mail.bradley.edu>
2024-08-20 03:17:22 -0500

    UI fixes

--- Project Gilbert 2024/Assets/Scripts/Player/PlayerItemChange.cs
+++ Project Gilbert 2024/Assets/Scripts/Player/PlayerItemChange.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.ShaderKeywordFilter;
+//using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 using UnityEngine.Playables;
 
